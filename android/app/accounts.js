873b184f9342daf2b7a8730613684b22614da60b
Pmaha-raja <147468927+Pmaha-raja@users.noreply.github.com>
2026-06-12 17:27:09 +0530

    Update accounts.js

--- android/app/accounts.js
+++ android/app/accounts.js
@@ -1,15 +1,1 @@
-export default async function handler(req, res) {
-  const supabaseUrl = 'https://nnrjievanuqlealabihr.supabase.co';
-  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ucmppZXZhbnVxbGVhbGFiaWhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0ODgzOTMsImV4cCI6MjA5NjA2NDM5M30.jz60L6ZQlPHmj22XkDGIxL1WMYEYHpkhYsKyQ8w3has';
-
-  const response = await fetch(`${supabaseUrl}/rest/v1/vkb_accounts?select=*`, {
-    headers: {
-      'apikey': supabaseKey,
-      'Authorization': `Bearer ${supabaseKey}`,
-      'Content-Type': 'application/json'
-    }
-  });
-
-  const data = await response.json();
-  res.status(200).json(data);
-}
+hi
