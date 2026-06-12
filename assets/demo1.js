24c528b04f6e34e672b7e2a351a00dc2c87ea725
Pmaha-raja <147468927+Pmaha-raja@users.noreply.github.com>
2026-06-12 17:14:00 +0530

    Update demo1.js

--- assets/demo1.js
+++ assets/demo1.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const supabaseUrl = 'https://nnrjievanuqlealabihr.supabase.co';
-  const supabaseKey = 'YOUR_ANON_KEY';
+  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ucmppZXZhbnVxbGVhbGFiaWhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0ODgzOTMsImV4cCI6MjA5NjA2NDM5M30.jz60L6ZQlPHmj22XkDGIxL1WMYEYHpkhYsKyQ8w3has';
 
   const response = await fetch(`${supabaseUrl}/rest/v1/vkb_accounts?select=*`, {
     headers: {
